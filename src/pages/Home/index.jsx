8fa07f01b077fb128f08103499ef35429b7cb839
Kagiso31 <kagisomokou@gmail.com>
2023-07-03 20:58:49 +0200

    Add home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,21 +1,31 @@
-/* imports  */
+import { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    document.body.classList.add("home-page");
+    return () => {
+      document.body.classList.remove("home-page");
+    };
+  }, []);
   return (
-    <section>
-      <div>
-        <h1>
-          <strong>So, you want to travel to</strong>
-          Space
-        </h1>
-        <p>
-          Let’s face it; if you want to go to space, you might as well genuinely
-          go to outer space and not hover kind of on the edge of it. Well sit
-          back, and relax because we’ll give you a truly out of this world
-          experience!
-        </p>
+    <section className="home">
+      <div className="wrapper">
+        <div className="home__text">
+          <h1>
+            <strong className="home__subtitle">
+              So, you want to travel to
+            </strong>
+            <strong className="home__title">Space</strong>
+          </h1>
+          <p className="home__body">
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <button className="home__button">Explore</button>
       </div>
-      <button>Explore</button>
     </section>
   );
 };
